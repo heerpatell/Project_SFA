270bb4634f0781ab72cfe7d1082ff4377d6208c1
heerpatell <68782787+heerpatell@users.noreply.github.com>
2024-09-06 01:45:08 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -119,6 +119,6 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-server.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
