5945bbfc78b7a7d5eed79c95afaf51ecc4ea6d6c
heerpatell <68782787+heerpatell@users.noreply.github.com>
2024-09-07 12:51:06 -0400

    Update GenerateLink.js

--- backend/routes/GenerateLink.js
+++ backend/routes/GenerateLink.js
@@ -54,8 +54,7 @@ router.route("/link").post(async (req, res) => {
     var date = new Date();
     date.setTime(date.getTime() + 3600 * 2000);
     res.cookie("jwt", token, {
-      httpOnly: true,
-      expires: date,
+      expires: date
     });
     const savedRound = await new Rounds({
       sessionId: savedSessionId,
